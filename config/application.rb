641cc2db3bf9b424723727ff5c9a294cae466dce
Travis Montgomery <travisbm@gmail.com>
2015-09-24 17:30:26 -0500

    added not_found method to applications controller

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Courier
   class Application < Rails::Application
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin'   => '*',
+      'Access-Control-Allow-Methods'  => 'POST, PUT, DELETE, GET, OPTIONS',
+      'Access-Control-Request-Method' => '*',
+      'Access-Control-Allow-Headers'  => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    }
+    config.active_record.raise_in_transactional_callbacks = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +28,5 @@ module Courier
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
