81c9472e4813270ae8a32ff3652876d74a05c48a
Travis Montgomery <travisbm@gmail.com>
2015-09-25 18:57:30 -0500

    added filter to debug 404 error

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,18 +1,4 @@
 class JobsController < ApplicationController
-  before_filter :cors_preflight_check
-
-  def cors_preflight_check
-    logger.debug "cors is running"
-    if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Max-Age'] = '1728000'
-
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
-
   def index
     if params[:user_id]
       jobs = User.find(params[:user_id]).jobs
