a5f29357ee264f5caa1c1e6dfdaf453e682ffa0d
Travis Montgomery <travisbm@gmail.com>
2015-09-25 11:31:53 -0500

    updated models user and job to has_one :address, instead of has_many

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ActiveRecord::Base
   belongs_to :user
   belongs_to :messenger
-  has_many   :addresses
+  has_one   :addresses
   validates :business_name, :location, :job_description, :phone, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :jobs
-  has_many :addresses
+  has_one :addresses
   validates :first_name, :last_name, :user_name, :business_name,
             :location, :phone, presence: true
 end
