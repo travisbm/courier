cce8a1530ba9fad73ce684e619c80c25644259df
Travis Montgomery <travisbm@gmail.com>
2015-09-25 17:34:48 -0500

    removed data validation for testing

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ActiveRecord::Base
   belongs_to :user
   belongs_to :job
-  validates :street_name, :city, :state, :zip, presence: true
+  # validates :street_name, :city, :state, :zip, presence: true
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,5 @@ class Job < ActiveRecord::Base
   belongs_to :user
   belongs_to :messenger
   has_many   :addresses, :dependent => :delete_all
-  validates :business_name, :location, :job_description, :phone, presence: true
+  # validates :business_name, :location, :job_description, :phone, presence: true
 end
