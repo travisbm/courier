03809746407a67e8fb1b1f10eca230d81af0b04d
Travis Montgomery <travisbm@gmail.com>
2015-09-25 11:38:33 -0500

    reversed changes to user and job from last push because they broke the app

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ActiveRecord::Base
   belongs_to :user
   belongs_to :messenger
-  has_one   :addresses
+  has_many   :addresses
   validates :business_name, :location, :job_description, :phone, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :jobs
-  has_one :addresses
+  has_many :addresses
   validates :first_name, :last_name, :user_name, :business_name,
             :location, :phone, presence: true
 end
