bd4ca878854f3e99937d2dd40fa27bbe98f15904
Travis Montgomery <travisbm@gmail.com>
2015-09-26 14:32:20 -0500

    updated methods in users controller to catch when records arent found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def index
-    users = User.all
-    render json: users.to_json, status: 200
+    if User.count.nil?
+      render json: { err_message: "No Users in queue." }, status: 404
+    else
+      users = User.all
+      render json: users.to_json, status: 200
+    end
   end
 
   def show
@@ -11,7 +15,7 @@ class UsersController < ApplicationController
                                                 :include => :addresses }
       }), status: 200
     else
-      render json: { err_message: "Record not found." }, status: 404
+      render json: { err_message: "User record not found." }, status: 404
     end
   end
 
