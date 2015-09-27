606298a90e24d66ff480a0f85207e1f7212c7cec
Travis Montgomery <travisbm@gmail.com>
2015-09-27 16:58:18 -0500

    migrated new column in jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924212528) do
+ActiveRecord::Schema.define(version: 20150927214717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150924212528) do
     t.integer  "messenger_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "status"
   end
 
   add_index "jobs", ["messenger_id"], name: "index_jobs_on_messenger_id", using: :btree
